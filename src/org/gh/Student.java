dae500b50e779d041a99608fbb2b75fa78bf4569
smithd23 <66260633+smithd23@users.noreply.github.com>
2022-11-29 09:48:36 -0600

    Update Student.java

--- src/org/gh/Student.java
+++ src/org/gh/Student.java
@@ -9,14 +9,21 @@ public class Student extends Person implements Printable{
     // attribute to store students grade 
     // NOTE: it's private, so we provide a getter and setter
     private String grade;
+    private String gpa;
     
     // set grade
     public void setGrade(String grade){
         this.grade = grade;
     }
+    public void setgpa(String gpa){
+        this.gpa = gpa;
+    }
     // get grade
     public String getGrade(){
         return this.grade;
+    }
+    public String getgpa(){
+        return this.gpa;
     }
     
     // constructor called to create a new student object
@@ -25,10 +32,19 @@ public class Student extends Person implements Printable{
         super(name);
         this.setGrade(grade);
     }
+    public Senior(String name, String gpa){
+        super(name);
+        this.setgpa(gpa);
+    }
+
 
     // Because this class implements Printable, I must override the printMe() method
     public String printMe(){
         // For a student, I want the name and grade to print
         return "Student details from PrintMe() - " + this.getName()+ " - "+this.getGrade();
+    }
+    public String printMe(){
+        // For a student, I want the name and grade to print
+        return "Senior details from PrintMe() - " + this.getName()+ " - "+this.getgpa();
     }
 }

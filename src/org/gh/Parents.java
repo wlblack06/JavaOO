75499c82eced39495724964c453166d3faa8ff5b
KFry101 <97113447+KFry101@users.noreply.github.com>
2022-11-29 09:39:19 -0600

    Update and rename Parents to Parents.java

---
+++ src/org/gh/Parents.java
@@ -0,0 +1,11 @@
+package org.gh;
+
+public class Parent extends Person implements Printable{
+  private String student;
+  
+  public Parent(String name, String student){
+    
+    super(name);
+    
+    this. 
+

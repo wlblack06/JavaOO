472e186c8c074a1e2fa66c7f76c4dc07508f7c36
smithd23 <66260633+smithd23@users.noreply.github.com>
2022-11-29 09:45:51 -0600

    Update RunMe.java

--- src/org/gh/RunMe.java
+++ src/org/gh/RunMe.java
@@ -11,12 +11,16 @@ public class RunMe{
         // see getName() and toString() below which are defined in Person
         System.out.println("Creating a new student and Teacher");
         Student objStudent = new Student("Student 1", "GradeA");
+        Senior objSenior = new Senior("Senior 1", "gpa#1");
         Teacher objTeacher = new Teacher("Teacher 1", "Class#1, Class#2");
 
 
         // Print details of a student we created above (name - grade)
         System.out.println("Print with name (from person) and grade (from student)");
         System.out.println(objStudent.getName() + " - " + objStudent.getGrade());
+
+        System.out.println("Print with name (from person) and gpa (from senior)");
+        System.out.println(objStudent.getName() + " - " + objStudent.getgpa());
 
 
         // Print details of a Teacher we created above (name - classes)
@@ -28,12 +32,15 @@ public class RunMe{
         System.out.println("Print the Student and teacher using the printMe method from Printable");
         System.out.println(objStudent.printMe());
         System.out.println(objTeacher.printMe());
+        System.out.println(objSenior.printMe());
+        
 
 
         // Use the toString method of Student (not overriden from the Parent definition)
         System.out.println("Use the toString method in person since it's not overridden in the subclasses");
         System.out.println(objStudent);
         System.out.println(objTeacher);
+        System.out.println(objSenior);
 
 
         // Create a new object and print it 

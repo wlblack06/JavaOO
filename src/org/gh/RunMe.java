e59d7419fe0a14b615d766d6f7c49b795bf58f01
wlblack06 <116006598+wlblack06@users.noreply.github.com>
2022-12-01 09:28:10 -0600

    Update RunMe.java

--- src/org/gh/RunMe.java
+++ src/org/gh/RunMe.java
@@ -12,6 +12,7 @@ public class RunMe{
         System.out.println("Creating a new student and Teacher");
         Student objStudent = new Student("Student 1", "GradeA");
         Teacher objTeacher = new Teacher("Teacher 1", "Class#1, Class#2");
+        Teacher objRacer = new Racer("Racer 1", "Car#");
 
 
         // Print details of a student we created above (name - grade)
@@ -23,17 +24,24 @@ public class RunMe{
         System.out.println("Print with name (from person) and Classes (from Teacher)");
         System.out.println(objTeacher.getName() + " - " + objTeacher.getClasses());
         
+        
+        System.out.println("Print with name (from person) and Classes (from Racer)");
+        System.out.println(objRacer.getTime() + " - " + objRacer.getTime());
+        
 
         // This uses the polymorphism to call printMe and see different results
         System.out.println("Print the Student and teacher using the printMe method from Printable");
         System.out.println(objStudent.printMe());
         System.out.println(objTeacher.printMe());
+        System.out.println(objRacer.printMe());
+
 
 
         // Use the toString method of Student (not overriden from the Parent definition)
         System.out.println("Use the toString method in person since it's not overridden in the subclasses");
         System.out.println(objStudent);
         System.out.println(objTeacher);
+        System.out.println(objRacer);
 
 
         // Create a new object and print it 
